7dcbee6949baa777a631216a6007ca00bf78b829
McDaniel <mcdansl1@mcdansl1-ml1.jhuapl.edu>
2019-01-24 13:12:43 -0500

    Adding files for cpu monitoring

---
+++ ApexCpuPerformance.h
@@ -0,0 +1,27 @@
+#ifndef APEXCPUPERFORMANCE_H
+#define APEXCPUPERFORMANCE_H
+
+#define ENABLE_APEXPERFORMANCE
+
+
+#define APEXPERFORMANCE_SHOWIMMEDIATE false
+
+#ifdef ENABLE_APEXPERFORMANCE
+
+#include <stdio.h>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class ApexCpuPerformance
+{
+  
+
+
+
+
+
+
+
+}
--- ApexPerformanceTimer.cpp
+++ ApexPerformanceTimer.cpp
@@ -1,12 +1,12 @@
 /* Author: Sean L. McDaniel-Gray
  *
- * The goal of this tool is to find hotspots all withouth using 
- * advanced frameworks or tools. 
+ * The goal of this tool is to find hotspots all withouth using
+ * advanced frameworks or tools.
  *
  * If you need to check the execution time of some code/system
- * just include a header ( and the cpp file) and use the 
+ * just include a header ( and the cpp file) and use the
  * APEXPERFORMANCETIMER_FUNCSTART or APEXPERFORMANCETIMER_FUNCSTART(str)/END
- * in the analyzed placed 
+ * in the analyzed placed
  */
 
 #include <iostream>
@@ -22,35 +22,35 @@ int ApexPerformanceTimer::s_indent = 0;
 
 duration<double> dummy_zero = high_resolution_clock::duration::zero();
 
-// The constructor adds a new entry into the static vector 
+// The constructor adds a new entry into the static vector
 ApexPerformanceTimer::ApexPerformanceTimer(const char* strText, bool bShowImmediate) : m_str(strText), m_bShowImmediate(bShowImmediate)
 {
-  // Takes the start time when the timer is first constructor 
+  // Takes the start time when the timer is first constructor
   t_start = high_resolution_clock::now();
 
-  // If showImmediate is false add data to the vector  
+  // If showImmediate is false add data to the vector
   if (!m_bShowImmediate)
     {
       m_id = s_data.size();
 
-      // adds new element to the end of the vector 
+      // adds new element to the end of the vector
       s_data.push_back(std::make_tuple(m_str, dummy_zero, s_indent));
 
       s_indent++;
     }
 }
 
-// The destructor 
+// The destructor
 ApexPerformanceTimer::~ApexPerformanceTimer()
 {
-  
-  // Takes the ending time of the timer 
+
+  // Takes the ending time of the timer
   t_stop = high_resolution_clock::now();
-  
-  // Computes the elapsed time using the start and end time 
+
+  // Computes the elapsed time using the start and end time
   t_elapsed = duration_cast<duration<double>>(t_stop - t_start);
 
-  // If showImmediate is true will print out the data immediately 
+  // If showImmediate is true will print out the data immediately
   if (m_bShowImmediate)
     {
       PERFORMANCE_PRINTF_FUNC(m_str, t_elapsed);
@@ -63,14 +63,14 @@ ApexPerformanceTimer::~ApexPerformanceTimer()
   --s_indent;
 }
 
-// Show the current data 
+// Show the current data
 void ApexPerformanceTimer::ReportAll()
 {
   for (const auto &d : s_data)
     {
       int indents = get<2>(d);
 
-      while (indents-- > 0) 
+      while (indents-- > 0)
 	PERFORMANCE_PRINTF_FUNC("  ");
 
       PERFORMANCE_PRINTF_FUNC(get<0>(d).c_str(), ((get<1>(d)).count()));
